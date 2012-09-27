fe4725bef64ec99aadda5351c82d704ed156658f
terrence tang <terrencetang@terrenceteki-MacBook-Air.local>
2012-09-27 17:17:57 +0800

    init

---
+++ web.js
@@ -0,0 +1,23 @@
+var express  = require('express');
+
+var app = express.createServer(express.logger());
+app.use(express.static(__dirname+"/assert"));
+app.get('/admin/parse', function(request, response) {
+      require("./parse.js").run();
+      response.send('Hello World!');
+});
+
+
+app.get('/tw', function(request, response) {
+      response.setHeader("Content-Type" , "charset=utf-8");
+      response.sendfile(__dirname+'/assert/index.html');
+});
+/*
+app.get('/css/all.css', function(request, response) {
+      response.sendfile(__dirname+'/assert/css/all.css');
+});
+*/
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+      console.log("Listening on " + port);
+});
